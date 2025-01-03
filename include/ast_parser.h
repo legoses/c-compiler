a4a61e0b67e6868dfa8ee921e8814e3636e85069
Kyle <stonewal7@outlook.com>
2025-01-02 21:10:44 -0700

    begin ast simplification

--- include/ast_parser.h
+++ include/ast_parser.h
@@ -8,19 +8,6 @@
 #include <string.h>
 
 
-int check_type(int t, int t1);
-int get_num_parameters(struct token *token_arr[], int pos, int len);
-int str_len(const char *str);
-int count_func_contents(struct token *token_arr[], int pos, int len);
-int populate_ast_function(struct token *token_arr[], int pos, AST *ast, int contents);
-AST* create_string(char *w, int len);
-AST* create_return();
-AST* create_ast_identifier(struct token *token);
-AST *ast_new(AST ast);
-int recurse_ast(struct token *token_arr[], int len, int count);
-AST* create_return_keyword(char *w, int len);
-AST *create_main_function(struct token *token_arr[], int pos, int len);
-int add_ast_contents(AST **ast_arr, AST *ast, int *pos, int len); // add contents to array. Makes sure position is within arr length
 int create_ast(struct token *token_arr[], int len);
 
 
