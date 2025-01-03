f66055cc4974af616a1f5bc25cb5030e48ce8fc7
Kyle <stonewal7@outlook.com>
2025-01-02 23:03:49 -0700

    begin ast simplification 2

--- src/ast_parser.c
+++ src/ast_parser.c
@@ -1,10 +1,17 @@
 #include <ast_parser.h>
 
 
+AST create_ast_main_function(struct token *token_arr[], int pos, int len) {
+    
+}
+
 
 int create_ast(struct token *token_arr[], int len) {
     for(int i = 0; i < len; i++) {
-
+        switch(token_arr[i]->name) {
+            case MAIN_IDENTIFIER:
+                
+        }
     }
     return 0;
 }
